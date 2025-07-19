0d64ed81a43ec3835a41200dff2ce3500561c30d
joshua_meekins <jos.meekins@gmail.com>
2025-07-18 22:36:51 -0400

    foundation

---
+++ __init__.py
@@ -0,0 +1,19 @@
+import os
+from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+from dotenv import load_dotenv
+
+load_dotenv()
+
+db = SQLAlchemy()
+
+def create_app():
+    app = Flask(__name__)
+    app.config['SQLALCHEMY_DATABASE_URI'] = os.getenv('DATABASE_URL')
+    app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+    db.init_app(app)
+
+    from .routes import bp as main_bp
+    app.register_blueprint(main_bp)
+
+    return app
